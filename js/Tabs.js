b0ab0338e9db7d635ac27ae01314397023a9314e
slava-gerts <vyacheslavgerts@gmail.com>
2025-01-06 18:22:26 +0100

    adds tabs js

--- js/Tabs.js
+++ js/Tabs.js
@@ -20,14 +20,29 @@ class Tabs {
     this.rootElement = rootElement
     this.buttons = this.rootElement.querySelectorAll(this.selectors.button)
     this.contents = this.rootElement.querySelectorAll(this.selectors.content)
+    this.maxTabIndex = this.buttons.length - 1
 
-    this.state = {
+    this.state = this.getProxyState({
       activeTabIndex: [...this.buttons]
         .findIndex(button => button.classList.contains(this.stateClasses.isActive)),
-      maxTabIndex: this.buttons.length - 1,
-    }
+    })
 
     this.bindEvents()
+  }
+
+  getProxyState = (initialState) => {
+    return new Proxy(initialState, {
+      get: (target, prop) => {
+        return target[prop]
+      },
+      set: (target, prop, value) => {
+        target[prop] = value
+
+        this.updateUI()
+
+        return true
+      }
+    })
   }
 
   updateUI() {
@@ -37,19 +52,74 @@ class Tabs {
       const isActive = i === activeTabIndex
 
       button.classList.toggle(this.stateClasses.isActive, isActive)
+      button.setAttribute(this.stateAttributes.ariaSelected, isActive.toString())
+      button.setAttribute(this.stateAttributes.tabIndex, isActive ? '0' : '-1')
+
       this.contents[i].classList.toggle(this.stateClasses.isActive, isActive)
     })
   }
 
+  activateTab(tabIndex) {
+    this.state.activeTabIndex = tabIndex
+    this.buttons[tabIndex].focus()
+  }
+
+  previousTab = () => {
+    const newTabIndex = this.state.activeTabIndex === 0 ?
+      this.maxTabIndex :
+      this.state.activeTabIndex - 1
+
+    this.activateTab(newTabIndex)
+  }
+
+  nextTab = () => {
+    const newTabIndex = this.state.activeTabIndex === this.maxTabIndex ?
+      0 :
+      this.state.activeTabIndex + 1
+
+    this.activateTab(newTabIndex)
+  }
+
+  firstTab = () => {
+    this.activateTab(0)
+  }
+
+  lastTab = () => {
+    this.activateTab(this.maxTabIndex)
+  }
+
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex
-    this.updateUI()
+  }
+
+  onKeyDown = (event) => {
+    const {code, metaKey} = event
+
+    let actionCode = code
+    if (metaKey && code === 'ArrowLeft') {
+      actionCode = 'MacHome'
+    }
+    if (metaKey && code === 'ArrowRight') {
+      actionCode = 'MacEnd'
+    }
+
+    const action = {
+      ArrowLeft: this.previousTab,
+      ArrowRight: this.nextTab,
+      Home: this.firstTab,
+      End: this.lastTab,
+      MacHome: this.firstTab,
+      MacEnd: this.lastTab,
+    }[actionCode]
+
+    action?.()
   }
 
   bindEvents() {
     this.buttons.forEach((button, i) => {
       button.addEventListener('click', () => this.onButtonClick(i))
     })
+    this.rootElement.addEventListener('keydown', this.onKeyDown)
   }
 }
 
