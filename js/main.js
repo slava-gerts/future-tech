8c24cefb9165d43e93e49d4d6956c7e382858945
slava-gerts <vyacheslavgerts@gmail.com>
2025-01-15 00:48:53 +0100

    adds reviews section styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
+import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
 
 new Header()
 new TabsCollection()
+
+defineScrollBarWidthCSSVar()
