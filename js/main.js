d6015cc20febb26742c112c58801ea5ac9300d1d
slava-gerts <vyacheslavgerts@gmail.com>
2025-02-10 22:13:25 +0100

    adds about section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
+import VideoPlayerCollection from './VideoPlayer.js'
 import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
 
 new Header()
 new TabsCollection()
+new VideoPlayerCollection()
 
 defineScrollBarWidthCSSVar()
